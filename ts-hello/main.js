cd06ca818b01bc899925cfad8ddce21b6bbc8b06
Dan Dumitriu <dandumitriu33@gmail.com>
2020-12-18 18:38:43 +0200

    Add var/let demo just like js

--- ts-hello/main.js
+++ ts-hello/main.js
@@ -1,5 +1,13 @@
-function log(message) {
-    console.log(message);
+function doSomething() {
+    for (var i = 0; i < 5; i++) {
+        console.log(i);
+    }
+    console.log('Finally (var i is not code block but function scoped): ' + i);
+    console.log('Finally (let i will be block scoped scoped like C# - better way): ');
 }
-var message = "Hello world!";
-log(message);
+doSomething();
+// function log(message){
+//     console.log(message);
+// }
+// var message = "Hello world!";
+// log(message);
--- ts-hello/main.ts
+++ ts-hello/main.ts
@@ -1,7 +1,17 @@
-function log(message){
-    console.log(message);
+function doSomething() {
+    for (var i = 0; i < 5; i++){
+        console.log(i);
+    }
+    console.log('Finally (var i is not code block but function scoped): ' + i);
+    console.log('Finally (let i will be block scoped scoped like C# - better way): ');
 }
+doSomething();
 
-var message = "Hello world!";
 
-log(message);
+// function log(message){
+//     console.log(message);
+// }
+
+// var message = "Hello world!";
+
+// log(message);
