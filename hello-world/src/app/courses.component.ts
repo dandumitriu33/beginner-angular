79f5d4679a06c663c5d419b81da9bc8a51f80d14
Dan Dumitriu <dandumitriu33@gmail.com>
2020-12-18 21:30:19 +0200

    Add directives *ngFor example

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -3,11 +3,18 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'courses', // <courses>
     // selection similar to css <div id="courses"> "#courses"
-    template: '<h2>{{ "Title: " + getTitle() }}</h2>'
+    template: `
+        <h2>{{ "Title: " + getTitle() }}</h2>
+        <ul>
+            <li *ngFor="let course of courses">
+                {{ course }}
+            </li>
+        </ul>
+        `        
 })
 export class CoursesComponent {
     title = "List of courses";
-
+    courses = ["course 1", "course 2", "course 3"];
     getTitle(){
         return this.title;
     }
