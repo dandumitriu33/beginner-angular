4a7619e01ea6b7abba77d204a6d0069751dd1fb1
Dan Dumitriu <dandumitriu33@gmail.com>
2020-12-18 21:09:14 +0200

    Add first Angular component

---
+++ hello-world/src/app/courses.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'courses', // <courses>
+    // selection similar to css <div id="courses"> "#courses"
+    template: '<h2>Courses</h2>'
+})
+export class CoursesComponent {
+
+}
