65710c4f67e724ca85f5630ad1b37f9c831ee7ab
Dan Dumitriu <dandumitriu33@gmail.com>
2020-12-18 21:25:56 +0200

    Add string interpolation/template example

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'courses', // <courses>
     // selection similar to css <div id="courses"> "#courses"
-    template: '<h2>Courses</h2>'
+    template: '<h2>{{ "Title: " + getTitle() }}</h2>'
 })
 export class CoursesComponent {
+    title = "List of courses";
 
+    getTitle(){
+        return this.title;
+    }
 }
