38a5ef13891e979f6c04cb24ed7b11d0ea1c4467
Dan Dumitriu <dandumitriu33@gmail.com>
2020-12-18 18:47:54 +0200

    Add variable declaration and enum

--- ts-hello/main.js
+++ ts-hello/main.js
@@ -1,11 +1,33 @@
-function doSomething() {
-    for (var i = 0; i < 5; i++) {
-        console.log(i);
-    }
-    console.log('Finally (var i is not code block but function scoped): ' + i);
-    console.log('Finally (let i will be block scoped scoped like C# - better way): ');
-}
-doSomething();
+var count = 5;
+// count = 'a'; // compilation error
+var b;
+b = 1;
+b = true;
+var c;
+// c = 'gg'; // compilation error
+var d;
+var f;
+var g;
+var h;
+var m = [1, 2, 3];
+var n = [1, true, 'a', false];
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+    Color[Color["Purple"] = 3] = "Purple";
+})(Color || (Color = {}));
+;
+var backgroundColor = Color.Red;
+// function doSomething() {
+//     for (var i = 0; i < 5; i++){
+//         console.log(i);
+//     }
+//     console.log('Finally (var i is not code block but function scoped): ' + i);
+//     console.log('Finally (let i will be block scoped scoped like C# - better way): ');
+// }
+// doSomething();
 // function log(message){
 //     console.log(message);
 // }
--- ts-hello/main.ts
+++ ts-hello/main.ts
@@ -1,11 +1,31 @@
-function doSomething() {
-    for (var i = 0; i < 5; i++){
-        console.log(i);
-    }
-    console.log('Finally (var i is not code block but function scoped): ' + i);
-    console.log('Finally (let i will be block scoped scoped like C# - better way): ');
-}
-doSomething();
+let count = 5;
+// count = 'a'; // compilation error
+let b;
+b = 1;
+b = true;
+let c: number;
+// c = 'gg'; // compilation error
+let d: number;
+let f: boolean;
+let g: string;
+let h: any;
+let m: number[] = [1, 2, 3];
+let n: any[] = [1, true, 'a', false];
+
+enum Color{ Red = 0, Green = 1, Blue = 2, Purple = 3 };
+let backgroundColor = Color.Red;
+
+
+
+
+// function doSomething() {
+//     for (var i = 0; i < 5; i++){
+//         console.log(i);
+//     }
+//     console.log('Finally (var i is not code block but function scoped): ' + i);
+//     console.log('Finally (let i will be block scoped scoped like C# - better way): ');
+// }
+// doSomething();
 
 
 // function log(message){
