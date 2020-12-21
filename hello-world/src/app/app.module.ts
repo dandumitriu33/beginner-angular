df568ec8eaf36bb91b1de915ac10b09852a1d20b
Dan Dumitriu <dandumitriu33@gmail.com>
2020-12-21 13:06:13 +0200

    Add authors service to app.module.ts

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { CoursesComponent } from './courses.component';
 import { CourseComponent } from './course/course.component';
 import { CoursesService } from './courses.service';
 import { AuthorComponent } from './author/author.component';
+import { AuthorsService } from './authors.service';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { AuthorComponent } from './author/author.component';
     AppRoutingModule
   ],
   providers: [
-    CoursesService // singleton
+    CoursesService, // singleton
+    AuthorsService
   ],
   bootstrap: [AppComponent]
 })
--- hello-world/src/app/authors.service.ts
+++ hello-world/src/app/authors.service.ts
@@ -6,4 +6,8 @@ import { Injectable } from '@angular/core';
 export class AuthorsService {
 
   constructor() { }
+
+  getAuthors() {
+    return ["John Smith", "Jim Doe", "Sue Marks"];
+  }
 }
